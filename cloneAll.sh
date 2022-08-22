67d5638cdcb2803e0d2c361de7e66c0541ca7b81
Frieder Pankratz <frieder.pankratz@gmail.com>
2022-08-22 17:16:21 +0200

    add script

---
+++ cloneAll.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+git clone git@github.com:traact/traact_run_env.git
+git clone git@github.com:traact/traact_core.git
+git clone git@github.com:traact/traact_spatial.git
+git clone git@github.com:traact/traact_vision.git
+git clone git@github.com:traact/dataflow_use_case_test.git
+git clone git@github.com:traact/traact_component_aruco.git
+git clone git@github.com:traact/traact_component_basic.git
+git clone git@github.com:traact/traact_component_cereal.git
+git clone git@github.com:traact/traact_component_http.git
+git clone git@github.com:traact/traact_component_kinect_azure.git
+git clone git@github.com:traact/traact_component_pcpd_shm.git
+git clone git@github.com:traact/traact_component_pointcloud.git
+git clone git@github.com:traact/traact_dataflow_test.git
+git clone git@github.com:traact/traact_gui.git
+git clone git@github.com:FriederPankratz/aruco_test_app.git
+git clone git@github.com:FriederPankratz/test_apps.git
