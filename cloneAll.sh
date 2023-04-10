38ceec6f0b840946682bd270d26296e275df7f14
Frieder Pankratz <frieder.pankratz@gmail.com>
2023-04-10 13:38:16 +0200

    traact gui not working, remove from repos for now

--- cloneAll.sh
+++ cloneAll.sh
@@ -13,6 +13,6 @@ git clone git@github.com:traact/traact_component_kinect_azure.git
 git clone git@github.com:traact/traact_component_pcpd_shm.git
 git clone git@github.com:traact/traact_component_pointcloud.git
 git clone git@github.com:traact/traact_dataflow_test.git
-git clone git@github.com:traact/traact_gui.git
+git clone git@github.com:traact/traact.git
 git clone git@github.com:FriederPankratz/aruco_test_app.git
 git clone git@github.com:FriederPankratz/test_apps.git
